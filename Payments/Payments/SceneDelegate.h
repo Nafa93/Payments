d03402fb4afc09ee738d48883c67bafec4bd850d
Nicolas Amorosino <namorosino@deviget.com>
2020-06-06 22:31:27 -0300

    Removed storyboard, added HomeViewController

---
+++ Payments/Payments/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Payments
+//
+//  Created by Nicolas Alejandro Fernandez Amorosino on 6/6/20.
+//  Copyright © 2020 Nicolas Alejandro Fernandez Amorosino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
